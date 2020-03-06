39853657aca87abf39af028ed8a3ccb50c39c894
Matt Coburn <mattcoburn24@gmail.com>
2020-03-06 00:03:54 -0800

    added timer Svg
    seperated timer class into timer.js added an circle svg to index.html and added the timer ticking down functionality to app.js

--- app.js
+++ app.js
@@ -1,52 +1,22 @@
-class Timer {
-	constructor(durationInput, startButton, pauseButton, callbacks) {
-		this.durationInput = durationInput;
-		this.startButton = startButton;
-		this.pauseButton = pauseButton;
-		if (callbacks) {
-			this.onStart = callbacks.onStart;
-		}
-		this.startButton.addEventListener('click', this.start);
-		this.pauseButton.addEventListener('click', this.pause);
-	}
-
-	start = () => {
-		if (this.onStart) {
-			this.onStart();
-		}
-		this.tick();
-		this.interval = setInterval(this.tick, 1000);
-	};
-
-	tick = () => {
-		if (this.timeRemaining <= 0) {
-			this.pause();
-		} else {
-			this.timeRemaining = this.timeRemaining - 1;
-		}
-	};
-
-	pause = () => {
-		console.log('FREEZE everybody clapp your cheeks');
-		clearInterval(this.interval);
-	};
-
-	get timeRemaining() {
-		return parseFloat(this.durationInput.value);
-	}
-
-	set timeRemaining(time) {
-		this.durationInput.value = time;
-	}
-}
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
 	onStart() {
 		console.log('timer started');
 	},
-	onTick() {},
-	onComplete() {}
+	onTick() {
+		currentOffset = currentOffset - 50;
+		circle.setAttribute('stroke-dashoffset', currentOffset);
+	},
+	onComplete() {
+		console.log('TIMES UP!!!');
+	}
 });
